b11e4abf3dd999582174b281c5ee3c8f70381edc
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-26 17:22:18 +0000

    Deploying to gh-pages from @ AnastasiyaSeroshtan/goit-js-hw-09@d853ff4d10de08f401d9800f59d0eb1cb51018f9 🚀

---
+++ 01-color-switcher.67e9b0ba.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),d=document.querySelector("button[data-stop]");let r=null;e.addEventListener("click",(function(){e.setAttribute("disabled","disabled"),d.removeAttribute("disabled"),r=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),d.addEventListener("click",(function(){clearInterval(r),e.removeAttribute("disabled"),d.setAttribute("disabled","disabled")})),d.setAttribute("disabled","disabled");
+//# sourceMappingURL=01-color-switcher.67e9b0ba.js.map
