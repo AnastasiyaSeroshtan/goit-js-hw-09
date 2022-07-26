d853ff4d10de08f401d9800f59d0eb1cb51018f9
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-26 20:21:49 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyEl = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+let timeId = null;
+
+btnStart.addEventListener('click', handleBtnStartClick);
+btnStop.addEventListener('click', handleBtnStopClick);
+btnStop.setAttribute('disabled', 'disabled');
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function handleBtnStartClick() {
+    btnStart.setAttribute("disabled", "disabled");
+    btnStop.removeAttribute('disabled');
+    timeId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000); 
+}
+
+function handleBtnStopClick() {
+    clearInterval(timeId);
+    btnStart.removeAttribute('disabled');
+    btnStop.setAttribute('disabled', 'disabled');
+}
